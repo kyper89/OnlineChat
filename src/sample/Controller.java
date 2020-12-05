75d245c847bba1ffa44874883cfc434b66e113fa
kyper89 <kyper89@mail.ru>
2020-12-05 22:10:52 +0300

    Lesson four homework

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField messageText;
+
+    @FXML
+    private TextArea messagesArea;
+
+    @FXML
+    private Button buttonEnter;
+
+    @FXML
+    public void buttonEnterClick(ActionEvent actionEvent) {
+        String enteredText = messageText.getText();
+        if (enteredText == "") {
+            return;
+        }
+        messagesArea.appendText(enteredText + "\n");
+        messageText.clear();
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
+    public void stop() throws Exception {
+        System.out.println("Stop");
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("Start");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Online chat");
+        primaryStage.setScene(new Scene(root, 800, 600));
+
         primaryStage.show();
     }
 
